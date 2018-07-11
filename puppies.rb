d5b8cd0b3d002864e64e29d60d470b16fa1e52e2
ssstroud23 <ssstroud23@gmail.com>
2018-07-11 15:47:43 +0000

    Done.

--- puppies.rb
+++ puppies.rb
@@ -1,14 +1,44 @@
 # 1. Write a method called no_puppies that takes in a number of puppies as an argument. If the number of puppies is greater than zero then this method should return "No more puppies!" otherwise it should return "Well done, Rachel!"
-
+def no_puppies(int)
+  if int > 0 
+    puts "No more puppies!"
+  else
+    puts "Good job, Rachel!"
+end 
+end
 
 
 # 2. Rachel's animal accumulation has been going well and we're going to cut her some slack. Write a new method called less_puppies that takes in a number of puppies as an argument and tells Rachel good job when she gets less than 3 puppies, to slow down if she's found exactly 3 puppies, and to get back on her animal restriction if she gets more than 3 puppies.
-
+def less_puppies(int)
+  if int < 3
+    puts "Good job, Rachel!"
+  elsif int == 3
+    puts "Slow down!"
+  else
+    puts "Get back to your animal restriction!"
+end
+end 
 
 
 # 3. We've decided to give Rachel a little more control over her puppy obsession. Write a method called some_puppies that takes in two arguments - the number of puppies she has found and a maximum puppy allowance. This method should tell Rachel good job if she gets less than half her maximum allowance and to get back on her puppy restriction when she exceeds her maximum.
-
-
+def some_puppies(int, max)
+  if int < max /2
+    puts "Good job!"
+  elsif int > max 
+    puts "Get back on puppy restriction!"
+    
+end
+end
+  
+  
 
 # BONUS
 # 4. Rachel has decided to track both her puppy allowance AND her cat allowance. Write a method called new_animal accumulation that takes in two arguments - number of puppies and number of cats. As long as she gets zero puppies OR zero cats this method should return "Good job!". If she gets 1 or more puppies AND 1 or more cats it should tell her she should really slow back down!
+def new_animal(puppies,cats)
+  if puppies == 0 || cats == 0 
+    puts "Good Job!"
+  else
+    puts "Slow back down!"
+    
+end
+end
